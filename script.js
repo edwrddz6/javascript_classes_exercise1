106fc51ccf8d0aaf07da3bbab185a47b69a00a90
Edward Diaz <diazedward24@gmail.com>
2023-11-14 18:00:25 -0800

    Add class in js

---
+++ script.js
@@ -0,0 +1,11 @@
+class student {
+    constructor(name, age, major) {
+      this.name = name;
+      this.age = age;
+      this.major = major;
+  }
+  }
+  
+  let newStudent = new student("Edward", 29, "Marketing");
+  
+  console.log(newStudent);
